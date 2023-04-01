44e3f00e1288e0d89ba8b8182ed2812045fbc3d2
Eduardo Walker <eduardo.olidev@gmail.com>
2023-04-01 17:22:04 -0400

    Feat: Add funcao buscar imagem do filme e criar sticker

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws Exception {
 
 
                         //Conexão HTTPS & Buscar Filmes
@@ -19,16 +19,19 @@ public class App {
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         String body = response.body();
-        //System.out.println(body);
+        System.out.println(body);
 
                          // Extração de dados  (Titilo, Ano, Classificação)
 
         var extrator = new JsonParse();
         var convertion = new Convertion();
+        var geradora = new StickerFactory();
+
+
         List<Map<String, String>> listaFilmes = extrator.parse(body);
 
         System.out.println("Tamanho da List: "+listaFilmes.size());
-       // System.out.println(listaFilmes.get(0));
+        System.out.println(listaFilmes.get(0));
 
 
                         // Exibir e manipular os dados.
@@ -40,10 +43,11 @@ public class App {
             System.out.println("Título: "+negrito+filme.get("title")+reset);
             System.out.println("Ano: "+negrito+filme.get("year")+reset);
             System.out.println("-------------------------------------------------------------------\n");
+            geradora.getUrlImg(filme.get("image"));
+            geradora.cria(filme.get("image"),filme.get("title"),filme.get("imDbRating"));
+
 
         }
-
-
 
     }
 
